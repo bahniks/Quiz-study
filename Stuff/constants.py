7ac4640a215411d5d501cf6a9aa4b87d5dcd467f
Stepan Bahnik <bahniks@seznam.cz>
2025-11-27 22:10:21 +0100

    Update constants.py

--- Stuff/constants.py
+++ Stuff/constants.py
@@ -1,6 +1,6 @@
 AUTOFILL = False
 TESTING = True
 GOTHROUGH = False #"Ending"
-URL = "https://dlab-bahnik.pythonanywhere.com/"
+URL = "https://bahnik.pythonanywhere.com/"
 URL = "TEST"
 #URL = "http://127.0.0.1:8000/"
